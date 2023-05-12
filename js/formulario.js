cc69470e3b27e7efba98c918396586eb49ca4150
spdrl48 <alanaperezdl48@gmail.com>
2023-05-12 11:38:59 -0600

    Validacon contraseñas Alan

--- js/formulario.js
+++ js/formulario.js
@@ -1,8 +1,7 @@
 function validar_registro(){
-	//validación del nombre
-	if (document.registro.nombre.value.length==0){
-		alert("Tiene que escribir su nombre")
-		document.registro.nombre.focus()
+	//validación de la contraseña
+	if (document.registro.contraseña.value.length==0 && document.registro.contraseña2.value.length==0){
+		alert("Los campos de contraseña no pueden estar vacios")
 		return 0;
 	}
 }
