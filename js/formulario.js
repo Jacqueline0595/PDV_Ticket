2a974bba8c05a9cded155a85bb3c616d1e09b6b5
spdrl48 <alanaperezdl48@gmail.com>
2023-05-13 11:47:32 -0600

    Comprobacion contraseñas iguales

--- js/formulario.js
+++ js/formulario.js
@@ -1,9 +1,16 @@
 function validar_registro(){
 	//validación de la contraseña
-	if (document.registro.contraseña.value.length==0 && document.registro.contraseña2.value.length==0){
+	if(document.registro.contraseña.value.length==0 && document.registro.contraseña2.value.length==0){
 		alert("Los campos de contraseña no pueden estar vacios")
 		return 0;
 	}
+	if(document.registro.contraseña.value == document.registro.contraseña2.value){
+		alert("Ambas contraseñas son iguales")
+	} else{
+		alert("Las contraseñas ingresadas son distintas")
+	}
+
+
 }
 
 // cambio de alancin
